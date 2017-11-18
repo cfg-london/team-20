df1f99de9d2a23950bce56d9c33099d7e639533d
Qais Patankar <qaisjp@gmail.com>
2017-11-18 09:40:44 +0000

    Fix admin logo design

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -4,8 +4,8 @@ class Admin extends Component {
     render() {
         return (
             <div style={{background: "rgb(102, 22, 108)", width: "100%", height: "100%", padding: "0px", margin: "0px", display: "flex", "justify-content": "center", "align-items": "center"}}>
-                <div style={{background: "rgb(132, 188, 179)", padding: "20px", "border-radius": "10px"}}>
-                    <img src="http://www.equalmeasures2030.org/wp-content/uploads/2017/10/em2030.jpg" style={{"text-align": "center", "max-width": "210px", height: "auto"}} />
+                <div style={{background: "rgb(116, 190, 179)", padding: "20px", "border-radius": "10px"}}>
+                    <img src="http://www.equalmeasures2030.org/wp-content/uploads/2017/10/em2030.jpg" style={{margin: "0 auto", "margin-bottom": "20px", display: "block", "max-width": "210px", height: "auto"}} />
                     <p>Upload your dataset here.</p>
                     <form action="https://jp-17-harjot1singh.c9users.io:8081/api/upload" method="post" enctype="multipart/form-data">
                         <input type="file" name="data" />
