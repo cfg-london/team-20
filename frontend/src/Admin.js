97332887e0b660ba68fc1d83adfa62b98651da2a
Qais Patankar <qaisjp@gmail.com>
2017-11-18 08:52:21 +0000

    Add upload form

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class Admin extends Component {
     render() {
         return (
-          <p>
-            Hello world!
-          </p>
+            <form action="https://jp-17-harjot1singh.c9users.io:8081/api/upload" method="post" enctype="multipart/form-data">
+                <input type="file" name="data" />
+                <input type="submit" value="Upload Image" name="submit" />
+            </form>
         );
     }
 }
